a680721e2a8850d0e2e1acaae735fe50bb742f5d
Brannen Hall <Brannen@HurtadoHall.me>
2017-01-21 23:26:55 -0600

    make window more complex

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-import pyaudio
-import wave
 import sys
+import alsaaudio
+import wave
 from PyQt5.QtWidgets import *
 
+#Since I am going to only be using this on one device, I hard coded the audio card name
+card = ''
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
@@ -12,4 +14,10 @@ if __name__ == '__main__':
     window = QWidget()
     window.setWindowTitle('Sample')
     window.adjustSize()
+    window.move(300, 300)
     window.show()
+
+    sys.exit(app.exec_())
+
+def main():
+    rec_dev = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK, card)
